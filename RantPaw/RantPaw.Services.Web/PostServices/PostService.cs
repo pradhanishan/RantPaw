6fd1ee11f0d2333c1c55e28e95ef4a12211a61b7
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2022-10-21 18:22:21 +0545

    Added skeleton newtonsoft deserialization

---
+++ RantPaw/RantPaw.Services.Web/PostServices/PostService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RantPaw.Models.DTOS.PostDTOS;
+using RantPaw.Models.ServiceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RantPaw.Services.Web.PostServices
+{
+    public sealed class PostService : IPostService
+    {
+
+        private readonly HttpClient _httpClient;
+
+
+        public PostService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ServiceResponse<List<GetPostDTO>>> GetAll()
+        {
+            var response = await _httpClient.GetAsync("/api/posts");
+            string responseAsString = await response.Content.ReadAsStringAsync();
+            ServiceResponse<List<GetPostDTO>> responseData = JsonConvert.DeserializeObject<ServiceResponse<List<GetPostDTO>>>(responseAsString)!;
+            return responseData;
+
+        }
+    }
+}
--- RantPaw/RantPaw.Web/Program.cs
+++ RantPaw/RantPaw.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RantPaw.Services.Web.PostServices;
 using RantPaw.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetSection("BaseAddress").Value) });
+
+// Web services
+
+builder.Services.AddScoped<IPostService, PostService>();
 
 await builder.Build().RunAsync();
